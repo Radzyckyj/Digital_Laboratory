9de1e0688c6f4b22d4297769796c8a68da8d1eef
Lucas Penna <35812528+lucaspenna00@users.noreply.github.com>
2019-03-26 14:49:22 -0300

    Add files via upload

--- hamming_error_top_level.vhd
+++ hamming_error_top_level.vhd
@@ -1,74 +1,89 @@
--- Polytechnic School of the University of Sao Paulo
--- Digital System and Computer Engineering Department
--- Digital Laboratory A - Experience 5
--- Authors: Lucas Penna Saraiva and Stefan R. Raposo
-
 entity hamming_error_top_level is
 
-    Port( input_binary:   in bit_vector(3 downto 0);
-            
-            flip:             in bit_vector(2 downto 0);    
-            output_binary:  out bit_vector(3 downto 0);
-            error_output:   out bit
-            );
+	Port( input_binary:   in bit_vector(3 downto 0);
+			
+			flip:    		 in bit_vector(2 downto 0);
+			
+			output_binary:  out bit_vector(3 downto 0);
+			
+			error_output:   out bit
+			
+			);
+
 end entity;
 
 architecture Behaviour of hamming_error_top_level is
 
 -- Components
 
-    -- Creating the Hamming Encoder component
+	-- Creating the Hamming Encoder component
 
-    component Hamming_Encoder74 is
-    
-        port( binary: in bit_vector(3 downto 0);
+	component Hamming_Encoder74 is
+	
+		port( binary: in bit_vector(3 downto 0);
 
-                hamming: out bit_vector(6 downto 0)
-             );
-    
-    end component;
-    
-    -- Creating the Hamming Decoder component
+				hamming: out bit_vector(6 downto 0)
+			 );
+	
+	end component;
+	
+	-- Creating the Hamming Decoder component
 
-    component decHamming is
-    
-        port ( hamming: in bit_vector(6 downto 0);
-                 
-                 binary: out bit_vector(3 downto 0); 
-               
-                 error: out bit 
-              
-              );
-                 
-    end component;
-    
+	component decHamming is
+	
+		port ( hamming: in bit_vector(6 downto 0);
+				 
+				 binary: out bit_vector(3 downto 0); 
+		       
+				 error: out bit 
+			  
+			  );
+				 
+	end component;
+	
 -- Signals
 
-    signal hamming_correct: bit_vector(6 downto 0);
+	signal hamming: bit_vector(6 downto 0);
 
-    signal hamming_error: bit_vector(6 downto 0);
-    
-    
+	signal hamming_aux: bit_vector(6 downto 0);
+	
+	
 -- Architecture
 
-    begin 
-    
-	 -- Encoding the binary_input
-	 
-    encoder: Hamming_Encoder74 port map(input_binary, hamming_correct);
-    
-    -- Inversion logic
-	 
-	 hamming_error(0) <= ( not(flip(2)) and not(flip(1)) and not(flip(0))) xor (hamming_correct(0));
-	 hamming_error(1) <= ( not(flip(2)) and not(flip(1)) and not(flip(0))) xor (hamming_correct(1));
-	 hamming_error(2) <= ( not(flip(2)) and not(flip(1)) and not(flip(0))) xor (hamming_correct(2));
-	 hamming_error(3) <= ( not(flip(2)) and not(flip(1)) and not(flip(0))) xor (hamming_correct(3));
-	 hamming_error(4) <= ( not(flip(2)) and not(flip(1)) and not(flip(0))) xor (hamming_correct(4));
-	 hamming_error(5) <= ( not(flip(2)) and not(flip(1)) and not(flip(0))) xor (hamming_correct(5));
-	 hamming_error(6) <= ( not(flip(2)) and not(flip(1)) and not(flip(0))) xor (hamming_correct(6));
-	 
-    -- Decoding the Hamming Code and attributting to the data output
-    
-	 decoder: decHamming port map(hamming_error, output_binary, error_output);
-            
+	begin 
+	
+	encoder: Hamming_Encoder74 port map(input_binary, hamming);
+	
+	
+	decoder: decHamming port map(hamming_aux, output_binary, error_output);
+
+	
+	-- Generating the Hamming Code for the data input
+	
+	
+	
+	-- Inversion logic //sequential
+	
+		hamming_aux(0) <= not(hamming(0)) when flip = "000" else
+		hamming(0); 																-- flipping the bits
+		
+		hamming_aux(1) <= not(hamming(1)) when flip = "001" else	 -- and creating the error
+		hamming(1);
+		
+		hamming_aux(2) <= not(hamming(2)) when flip = "010" else
+		hamming(2);
+		
+		hamming_aux(3) <= not(hamming(3)) when flip = "011" else
+		hamming(3);
+		
+		hamming_aux(4) <= not(hamming(4)) when flip = "100" else
+		hamming(4);
+		
+		hamming_aux(5) <= not(hamming(5)) when flip = "101" else
+		hamming(5);
+		
+		hamming_aux(6) <= not(hamming(6)) when flip = "110" else
+		hamming(6);
+	
+			
 end Behaviour;
